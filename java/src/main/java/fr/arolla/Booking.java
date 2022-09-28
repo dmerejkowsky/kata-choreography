4455369143140d37913c50699085e31298c8c0c4
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-09-28 14:57:07 +0200

    Orchestrate everything from the Booking class

--- java/src/main/java/fr/arolla/Booking.java
+++ java/src/main/java/fr/arolla/Booking.java
@@ -1,7 +1,17 @@
 package fr.arolla;
 
 public class Booking {
+  private final Inventory inventory;
+  private final Ticketing ticketing;
+
+  public Booking(Inventory inventory, Ticketing ticketing) {
+    this.inventory = inventory;
+    this.ticketing = ticketing;
+  }
+
   public void book(int numSeats) {
     System.out.format("Booking %d seats", numSeats);
+    inventory.decrementCapacity(numSeats);
+    ticketing.printTicket(numSeats);
   }
 }
--- java/src/test/java/fr/arolla/BookingTests.java
+++ java/src/test/java/fr/arolla/BookingTests.java
@@ -2,10 +2,17 @@ package fr.arolla;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookingTests {
   @Test
   void canBookSomeSeats() {
-    var booking = new Booking();
+    int totalSeats = 100;
+    var inventory = new Inventory(totalSeats);
+    var ticketing = new Ticketing();
+    var booking = new Booking(inventory, ticketing);
     booking.book(4);
+
+    assertEquals(96, inventory.getCapacity());
   }
 }
